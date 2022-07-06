cd32fad795604c63571eba0e63972301082ebd36
Darmellah Abdellatif <76949906+abdellatifdarmellah@users.noreply.github.com>
2022-07-06 01:07:57 +0100

    Create MathMagic.kt

---
+++ MathMagic.kt
@@ -0,0 +1,24 @@
+fun main() {
+  // declaring an immutable variable and assinging it to my name
+  val userName = "Abdellatif"
+
+  // declaring a magicNum without initalizing it
+  var magicNum: Int
+
+  // multiplying the first digit of my age by 2
+  magicNum = 2 * 5
+
+  // adding 3 to the result
+  magicNum += 3
+
+  // multiplying the result by 2
+  magicNum *= 2
+
+  // adding the second digit of my age to the result
+  magicNum += 1
+
+  // subtracting 6 from the result
+  magicNum -= 6
+
+  println("${userName}'s age is $magicNum")
+}
